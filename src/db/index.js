ae0d55ac75531b6a4a2dbbbcdc0cd62a3a43e0ce
ToxyFlog <52126367+ToxyFlog@users.noreply.github.com>
2022-04-03 10:56:33 +0300

    change

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ const {Pool} = require("pg");
 const client = new Pool({
 	max: 20,
 	connectionString: process.env.DATABASE_URL || "postgresql://root:root@localhost/trelo",
-	ssl: process.env.PRODUCTION ? {rejectUnauthorized: false} : false,
+	ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false,
 });
 
 client.on("error", e => {
