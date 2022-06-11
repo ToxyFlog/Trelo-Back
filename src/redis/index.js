02e0aa8d28fc6e9ee68f2a491a7b19c086db7ea9
ToxyFlog <52126367+ToxyFlog@users.noreply.github.com>
2022-06-11 11:20:26 +0300

    Update index.js

--- src/redis/index.js
+++ src/redis/index.js
@@ -2,7 +2,7 @@ const {createClient} = require("redis");
 
 if (process.env.NODE_ENV !== "test") {
 	const client = createClient({
-		url: process.env.REDISCLOUD_URL.match("(redis:\/\/).{1,20}:(.*@.*:[0-9]*\/)").slice(1).join(""),
+		url: process.env.REDISCLOUD_URL.match("(redis:\/\/)\S+:(.*@.*:[0-9]{1,6})").slice(1).join(""),
 	});
 	console.log("Redis is connected!");
 
